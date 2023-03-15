9977a2d8c208accdbc439df3b24307ebf8d78bca
Gmurad735 <abd.gm90@yandex.ru>
2023-03-15 21:35:01 +0300

    add simple fastapi examples

---
+++ fast_api_examples/fast_api_2.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"itme_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return fake_items_db
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip+limit]
